51e44ae406dcc434e8e91a6c3ef7f70cec1ce30f
VGizdic <vgizdic@pmfst.hr>
2026-03-18 14:27:15 +0100

    NADOKNADA

--- vjezbe/vjezbe1/kinematika.py
+++ vjezbe/vjezbe1/kinematika.py
@@ -1,32 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def jednoliko_gibanje(F, m, t_max=10, broj_tocka=100):
+def jednoliko_gibanje(F, m):
 
     a = F / m
-    t = np.linspace(0, t_max, broj_tocka)
+    t = np.linspace(0, 10, 100)
 
     v = a * t
     x = 0.5 * a * t**2
 
-    plt.figure(figsize=(10,8))
 
     plt.subplot(3,1,1)
-    plt.plot(t, x, 'b')
+    plt.plot(t, x, color='blue')
     plt.title("x - t graf")
     plt.xlabel("t (s)")
     plt.ylabel("x (m)")
     plt.grid(True)
 
     plt.subplot(3,1,2)
-    plt.plot(t, v, 'g')
+    plt.plot(t, v, color='green')
     plt.title("v - t graf")
     plt.xlabel("t (s)")
     plt.ylabel("v (m/s)")
     plt.grid(True)
 
     plt.subplot(3,1,3)
-    plt.axhline(y=a, color='r')
+    plt.plot(t, [a]*len(t), color='red')
     plt.title("a - t graf")
     plt.xlabel("t (s)")
     plt.ylabel("a (m/s²)")
