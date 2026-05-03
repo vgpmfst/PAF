d4749a8deb4ec310bc51a7a85a2317b377bae80f
VGizdic <vgizdic@pmfst.hr>
2026-05-03 19:37:14 +0200

    Vjezbe4 ispravak

--- vjezbe/vjezbe4/zadatak.py
+++ vjezbe/vjezbe4/zadatak.py
@@ -1,14 +1,3 @@
-#Newton - Lorentzova jednadžba: F = ma = q(E + v*B)
-#Položaj: r = (x,y,z), Brzina v: v= (vx,vy,vz), Akceleracija: a = (dvx/dt,dvy/dt,dvz/dt)
-#za B = (0,0,B) i E = (Ex,Ey,Ez), vektroski produkt v x B = (vyB,-vxB,0)
-#sustav diferencijalnih jednadžbi: dvx/dt = (q/m)(Ex + vyB), dvy/dt = (q/m)(Ey - vxB), dvz/dt = (q/m)Ez
-#Kada je E = 0, v0 = (vx0,vy0,vz0)-->
-# -> Gibanje u z smjeru: z(t) = z0 + vz0*t
-# -> Gibanje u xy ravnini: Kombinacijom prve dvije jednadžbe dobivamo opis kružnog gibanja s kutnom frekvencijom
-# ω = qB/m
-#Rješenje za x i y komponente su harmonijske funkcije. Rezultat je uniformno kružno gibanje u $xy$-ravnini.
-#Kombinacijom jednolikog gibanja po pravcu (z-os) i kružnog gibanja (xy-ravnina), čestica iscrtava helikoidnu putanju (spiralu)
-#Elektron i pozitron će imati istu putanju samo u različitom smjeru.
 import numpy as np
 import matplotlib.pyplot as plt
 
